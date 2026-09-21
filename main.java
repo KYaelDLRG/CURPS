644de41871bcccaaa6b22f3ed67a8bfe3d238b3e
KYaelDLRG <102941170+KYaelDLRG@users.noreply.github.com>
2026-09-21 10:46:13 -0600

    Inicializar repositorio con generador de CURPs

---
+++ main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class Main {
+    static int num = 15;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < num; i++)
+            System.out.println("CURP = " + getCURP());
+    }
+
+    static String getCURP() {
+        String Letra = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String Numero = "0123456789";
+        String Sexo = "HM";
+        String Entidad[] = { "AS", "BC", "BS", "CC", "CS", "CH", "CL", "CM", "DF", "DG", "GT",
+                "GR", "HG", "JC", "MC", "MN", "MS", "NT", "NL", "OC", "PL", "QT", "QR", "SP", "SL", "SR", "TC",
+                "TL", "TS", "VZ", "YN", "ZS" };
+        int indice;
+
+        StringBuilder sb = new StringBuilder(18);
+
+        for (int i = 1; i < 5; i++) {
+            indice = (int) (Letra.length() * Math.random());
+            sb.append(Letra.charAt(indice));
+        }
+
+        for (int i = 5; i < 11; i++) {
+            indice = (int) (Numero.length() * Math.random());
+            sb.append(Numero.charAt(indice));
+        }
+
+        indice = (int) (Sexo.length() * Math.random());
+        sb.append(Sexo.charAt(indice));
+
+        sb.append(Entidad[(int) (Math.random() * 32)]);
+
+        for (int i = 14; i < 17; i++) {
+            indice = (int) (Letra.length() * Math.random());
+            sb.append(Letra.charAt(indice));
+        }
+
+        for (int i = 17; i < 19; i++) {
+            indice = (int) (Numero.length() * Math.random());
+            sb.append(Numero.charAt(indice));
+        }
+
+        return sb.toString();
+    }
+}
